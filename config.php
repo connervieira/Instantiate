c55cf051eacb52fe891959d1030555582beca28e
Conner Vieira <cvieira@v0lttech.com>
2024-07-16 17:00:11 -0400

    Added basic config back-end

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+$instantiate_config = array();
+$instantiate_config["branding"]["product_name"] = "Instantiate";
+$instantiate_config["archive"]["path"] = "/home/cvieira/Archive/Videos/Social/Instagram/InstagramPersonal/";
+?>
